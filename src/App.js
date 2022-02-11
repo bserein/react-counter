f7c78d0b753b2d0d59cb2659ff9c818e1d82ff63
Brian S <brians@Brians-MacBook-Air.local>
2022-02-11 09:08:31 -0500

    adding more stuff to learn state and destructuring better

--- src/App.js
+++ src/App.js
@@ -3,14 +3,20 @@ import './App.css';
 
 function App() {
   const [counter, setCounter] = useState(0);
+  const [cars, setCars] = useState(['Audi', 'Tesla', 'Ford']);
+  const [person, setPerson] = useState({ name: 'Jeremiah', age: 31, shirt: 'tan' });
   return (
     <div className="App">
       <header className="App-header">
         
         <h1> Counter: {counter}</h1>
-      
+        <h2>Hello {person.name}</h2>
+       <h2>{cars.toString()}</h2>
+      {/* adding new buttons to get the  */}
         
         <div className='buttonContainer'>
+        <button onClick={() => setCars([...cars, 'Kia'])}>Add Kia</button>&nbsp;
+       
         <button
           onClick={() => {setCounter(counter + 1)}}
         > + </button>
