a52e9ea25f59ab8b2ebdb5945af29a7b000866c7
Brian S <brians@Brians-MacBook-Air.local>
2022-02-12 14:50:06 -0500

    adding a short circuit

--- src/App.js
+++ src/App.js
@@ -3,19 +3,19 @@ import './App.css';
 
 function App() {
   const [counter, setCounter] = useState(0);
-  const [cars, setCars] = useState(['Audi', 'Tesla', 'Ford']);
-  const [person, setPerson] = useState({ name: 'Jeremiah', age: 31, shirt: 'tan' });
+  // const [cars, setCars] = useState(['Audi', 'Tesla', 'Ford']);
+  // const [person, setPerson] = useState({ name: 'Jeremiah', age: 31, shirt: 'tan' });
   return (
     <div className="App">
       <header className="App-header">
         
         <h1> Counter: {counter}</h1>
-        <h2>Hello {person.name}</h2>
-       <h2>{cars.toString()}</h2>
+        {/* <h2>Hello {person.name}</h2>
+       <h2>{cars.toString()}</h2> */}
       {/* adding new buttons to get the  */}
         
         <div className='buttonContainer'>
-        <button onClick={() => setCars([...cars, 'Kia'])}>Add Kia</button>&nbsp;
+        {/* <button onClick={() => setCars([...cars, 'Kia'])}>Add Kia</button>&nbsp; */}
        
         <button
           onClick={() => {setCounter(counter + 1)}}
@@ -24,10 +24,11 @@ function App() {
         onClick={() => {setCounter(0)}}>
           reset
         </button>
-        <button 
+        {counter > 0 &&
+        <button
         onClick={() => {setCounter(counter - 1)}}>
           -
-        </button>
+        </button>}
         </div>
       </header>
     </div>
---
+++ src/Separator.js
@@ -0,0 +1,14 @@
+import react from "react";
+
+
+function Separator(counter){
+    if (counter > 1) (
+        <>
+        <button>
+        </button>
+        </>
+
+    )
+}
+
+export default Separator;
